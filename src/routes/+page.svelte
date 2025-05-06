e5a3378e7f29c9cda0a9fc2ec8eabdea29ba661d
Na He Jeon <nahejeon@JamBookAir.local>
2025-05-07 02:08:43 +0900

    Basic add new vocab modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+  import AddVocabModal from './AddVocabModal.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+  let showModal = $state(false);
 </script>
 
 <div class="flex flex-row">
   <div class="card bg-base-100 card-border min-w-50 m-1">
     <div class="card-body">
-      Add new
+        <!-- Open the modal using ID.showModal() method -->
+        <button class="btn" onclick={() => (showModal = true)}>open modal</button>
+        <AddVocabModal
+          bind:showModal
+        />
     </div>
   </div>
   {#each data.vocab_list as vocab}
---
+++ src/routes/AddVocabModal.svelte
@@ -0,0 +1,36 @@
+<script>
+	let { showModal = $bindable()} = $props();
+
+	let dialog = $state(); // HTMLDialogElement
+
+	$effect(() => {
+		if (showModal) dialog.showModal();
+	});
+</script>
+
+<dialog
+  id="add-vocab"
+  class="modal"
+  bind:this={dialog}
+  onclose={() => (showModal = false)}
+	onclick={(e) => { if (e.target === dialog) dialog.close(); }}
+>
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">Add new vocab</h3>
+    <fieldset class="fieldset">
+      <legend class="fieldset-legend">Vocab</legend>
+      <input type="text" class="input"/>
+      <legend class="fieldset-legend">Meaning</legend>
+      <input type="text" class="input"/>
+      <legend class="fieldset-legend">Notes</legend>
+      <textarea class="textarea" placeholder="Example sentences, pronunciation, etc."></textarea>
+    </fieldset>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Save</button>
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
