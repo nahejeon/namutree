57877b10999533159ac0272cdb943d2f27ff42e0
Na He Jeon <nahejeon@JamBookAir.local>
2025-06-24 01:19:21 +0900

    Creating and updating in folder page stays on folder page

--- src/routes/folder/[folder_id]/+page.server.ts
+++ src/routes/folder/[folder_id]/+page.server.ts
@@ -30,3 +30,68 @@ export const load: PageServerLoad = async ({ locals: { supabase }, params: { fol
 
   return { items, count, page, folder_id };
 }
+
+export const actions = {
+  new: async ({ request, locals: { supabase }, url }) => {
+    console.log(url);
+
+    const data = await request.formData();
+
+    const name = data.get('name');
+    const meaning = data.get('meaning');
+    const notes = data.get('notes');
+    const folderId = data.get('folder_id');
+
+    const { data: { user } } = await supabase.auth.getUser();
+
+    const { error } = await supabase
+      .from('items')
+      .insert([
+        { 
+          profile_id: user?.id,
+          name: name,
+          meaning: meaning,
+          notes: notes,
+          folder_id: folderId || null
+        },
+      ])
+      .select();
+
+    if (error) {
+      console.error(error);
+      redirect(303, url.pathname);
+    } else {
+      redirect(303, url.pathname);
+    } 
+  },
+
+  update: async ({ request, locals: { supabase }, url }) => {
+    const data = await request.formData();
+
+    const id = data.get('id');
+    const name = data.get('name');
+    const meaning = data.get('meaning');
+    const notes = data.get('notes');
+    const folderId = data.get('folder_id');
+
+    const { error } = await supabase
+      .from('items')
+      .update([
+        { 
+          name: name,
+          meaning: meaning,
+          notes: notes,
+          folder_id: folderId || null
+        },
+      ])
+      .eq('id', id)
+      .select();
+
+    if (error) {
+      console.error(error);
+      redirect(303, url.pathname);
+    } else {
+      redirect(303, url.pathname);
+    } 
+  },
+} satisfies Actions;
